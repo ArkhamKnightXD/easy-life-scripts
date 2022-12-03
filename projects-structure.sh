11466c4d734049c97a65466751dbb6545abfcd0e
Knight <karvin_jimenez@hotmail.com>
2022-12-02 20:47:20 -0400

    proyecto inicializado

---
+++ projects-structure.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# A simple Bash Script To Generate My Project code Structure
+
+echo Iniciando Script
+
+cd C:\
+
+mkdir Projects 
+
+cd Projects
+
+mkdir Backend-Development
+mkdir Frontend-Development
+mkdir Game-Development
+mkdir Mobile-Development
+
+cd Mobile-Development
+
+git clone https://github.com/ArkhamKnightXD/shopping-mobile-app.git
+git clone https://github.com/ArkhamKnightXD/testing-react-native.git
+git clone https://github.com/ArkhamKnightXD/testing-flutter.git
+
+cd ..
+cd Game-Development
+
+git clone https://github.com/ArkhamKnightXD/pong-game.git
+git clone https://github.com/ArkhamKnightXD/journey.git
+git clone https://github.com/ArkhamKnightXD/smb-clone.git
+git clone https://github.com/ArkhamKnightXD/libgdx-test.git
+git clone https://github.com/ArkhamKnightXD/learning-godot.git
+git clone https://github.com/ArkhamKnightXD/libgdx-platformer.git
+git clone https://github.com/ArkhamKnightXD/gdx-playground.git
+git clone https://github.com/ArkhamKnightXD/snake-game.git
+
+cd ..
+cd Backend-Development
+
+git clone https://github.com/ArkhamKnightXD/shipping-api.git
+git clone https://github.com/ArkhamKnightXD/loan-api.git
+git clone https://github.com/ArkhamKnightXD/secure-app.git
+git clone https://github.com/ArkhamKnightXD/covid-19-app.git
+git clone https://github.com/ArkhamKnightXD/rae-api.git
+git clone https://github.com/ArkhamKnightXD/PW-PRACTICE-HTTP.git
+git clone https://github.com/ArkhamKnightXD/arkham-knight-practica13.git
+git clone https://github.com/ArkhamKnightXD/breaking-bad-app.git
+git clone https://github.com/ArkhamKnightXD/game-changer.git
+git clone https://github.com/ArkhamKnightXD/game-changer-backend-v2.git
+git clone https://github.com/ArkhamKnightXD/spring-boot-ontology-test.git
+
+cd ..
+cd Frontend-Development
+
+git clone https://github.com/ArkhamKnightXD/game-changer-frontend-v2.git
+git clone https://github.com/ArkhamKnightXD/react-type.git
+git clone https://github.com/ArkhamKnightXD/gallery-album.git
+git clone https://github.com/ArkhamKnightXD/extra-practice.git
+git clone https://github.com/ArkhamKnightXD/learning-material-ui.git
+git clone https://github.com/ArkhamKnightXD/memory-app.git
+git clone https://github.com/ArkhamKnightXD/template.git
+git clone https://github.com/ArkhamKnightXD/res-ui-ux.git
+git clone https://github.com/ArkhamKnightXD/arkhamknightxd.github.io.git
+git clone https://github.com/ArkhamKnightXD/loan-frontend.git
+
+echo Proceso Completado
+
+
