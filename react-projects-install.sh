726f01fe898c2f117641fc50ca58e9c3f4d15634
karvin <karvin.jimenez@alternard.com>
2022-12-05 14:00:02 -0400

    Creado script para instalacion de dependencias de proyectos react

---
+++ react-projects-install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# A simple Bash Script To Generate My Different Projects Folder Structure
+
+echo Iniciando Script
+
+cd C:/Projects/Frontend-Development/
+
+
+echo Descargando dependencias de los Proyectos Frontend-Development
+
+cd game-changer-frontend-v2
+yarn install
+
+cd ..
+
+cd react-type
+npm install
+
+cd ..
+
+cd gallery-album
+yarn install
+
+cd ..
+
+cd extra-practice
+npm install
+
+cd ..
+
+cd learning-material-ui
+npm install
+
+cd ..
+
+cd memory-app
+npm install
+
+cd ..
+
+cd template
+npm install
+
+cd ..
+
+cd res-ui-ux
+npm install
+
+cd ..
+
+cd loan-frontend
+npm install
+
+echo instalacion de dependencias de proyectos Frontend-Development concluida
+
+cd ..
+
+cd ..
+
+cd Mobile-Development
+
+echo instalacion de dependencias de proyectos Mobile-Development
+
+cd testing-react-native
+yarn install
+
+cd ..
+
+cd shopping-mobile-app
+yarn install
+
+echo instalacion de dependencias de proyectos Mobile-Development concluida
+
+echo Proceso Completado
+
+
