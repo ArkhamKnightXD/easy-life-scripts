408db442a7522698b44235dfa6ea56d1630dc707
karvin <karvin.jimenez@alternard.com>
2023-01-12 14:40:58 -0400

    Mejorado script de instalacion de ros

--- ros-install.sh
+++ ros-install.sh
@@ -8,9 +8,11 @@ cd ~
 sudo apt update
 sudo apt upgrade -y
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc)main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudoapt-key add –
+sudo apt install curl -y
+
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 sudo apt update
 sudo apt install ros-noetic-desktop-full -y
